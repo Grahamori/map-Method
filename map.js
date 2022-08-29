248ad9cec2397a0c5b629a0b6112778f63e50ad0
Grahamori <grahamori@yahoo.co.uk>
2022-08-29 08:15:09 +0000

    Map method video complete

--- map.js
+++ map.js
@@ -5,15 +5,29 @@
  */
 
 // Using a for loop
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results) //<-- will result all numbers times two
 
 
 // Using map()
-
+const multByTwo = function (num) {
+  return num * 2;
+}
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
+const simpleResult = nums.map(function (num) {return num * 2}); //<-- Same as using map but simplified
+console.log(simpleResult); 
 
 
 // Simplfied w/ map() + arrow function
+const arrow = nums.map(num => num * 2); //<-- Same as above but includes an arrow function to make it shorter/more simplified
+console.log(arrow);
 
 
 // With objects:
@@ -37,3 +51,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id]);
+console.log(studentsWithIds);
